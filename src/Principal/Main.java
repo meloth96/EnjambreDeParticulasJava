71908e33096e9b7ea714628b0ef08e1e8d9a1f51
José Manuel Meza Valenciano <meloth96@gmail.com>
2018-03-14 23:42:48 -0600

    Se creo la clase Main

---
+++ src/Principal/Main.java
@@ -0,0 +1,11 @@
+package Principal;
+
+import Interfaz.VentanaPrincipal;
+
+public class Main {
+    
+    public static void main(String args[]){
+        new VentanaPrincipal().setVisible(true);
+    }
+    
+}
