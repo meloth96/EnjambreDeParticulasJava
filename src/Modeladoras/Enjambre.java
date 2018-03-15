cd115bbfc558481dc548f2f18215c6965c0b62a4
José Manuel Meza Valenciano <meloth96@gmail.com>
2018-03-15 10:32:02 -0600

    Se da valor a las constantes en tiempo de ejecucion en base a los datos ingresados por el usuario

--- src/Modeladoras/Enjambre.java
+++ src/Modeladoras/Enjambre.java
@@ -7,11 +7,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Enjambre {
 
     final static int RANGO_NUMEROS_ALEATORIOS = 5;
-    final static float ACELERACION_MINIMA = (float) 0.5;
-    final static float ACELERACION_MAXIMA = (float) 2.5;
-    final static float INERCIA_MINIMA = (float) 0.4;
-    final static float INERCIA_MAXIMA = (float) 0.9;
-    
+//    final static float ACELERACION_MINIMA = (float) 0.5;
+//    final static float ACELERACION_MAXIMA = (float) 2.5;
+//    final static float INERCIA_MINIMA = (float) 0.4;
+//    final static float INERCIA_MAXIMA = (float) 0.9;
+    static float ACELERACION_MINIMA;
+    static float ACELERACION_MAXIMA;
+    static float INERCIA_MINIMA;
+    static float INERCIA_MAXIMA;
+
     private float velocidadInercial;
     private float aceleracionMinima;
     private float aceleracionMaxima;
@@ -20,108 +24,119 @@ public class Enjambre {
     private int cantidadParticulas;
     private Particula lider;
 
-    
     private int funcionObjetivo(int x, int y) {
         return (int) (Math.pow(x, 2) + Math.pow(y, 2));
     }
 
-    public Enjambre(int cantidadParticulas) {
+    public Enjambre(int cantidadParticulas, float aMin, float aMax, float inMin, float inMax, boolean velocidadAleatoria) {
+        ACELERACION_MINIMA = aMin;
+        ACELERACION_MAXIMA = aMax;
+        INERCIA_MAXIMA = inMax;
+        INERCIA_MINIMA = inMin;
         this.cantidadParticulas = cantidadParticulas;
         particulas = new ArrayList<>(cantidadParticulas);
         generarPosicionesAleatorias();
+        if (velocidadAleatoria) {
+            generarVelocidadesAleatorias();
+        }
         lider = getParticulaMejorPosicion();
     }
 
     private void generarPosicionesAleatorias() {
         int posicionX, posicionY;
         for (int i = 0; i < cantidadParticulas; i++) {
-            posicionX = ThreadLocalRandom.current().nextInt(40, 90+1);
-            posicionY = ThreadLocalRandom.current().nextInt(40,90+1);
-            
-//            posicionX = ThreadLocalRandom.current().nextInt(-100, 100+1);
-//            posicionY = ThreadLocalRandom.current().nextInt(-100,100+1);
-            particulas.add(new Particula(posicionX,posicionY));
+            posicionX = ThreadLocalRandom.current().nextInt(40, 90 + 1);
+            posicionY = ThreadLocalRandom.current().nextInt(40, 90 + 1);
+            particulas.add(new Particula(posicionX, posicionY));
+        }
+    }
+
+    private void generarVelocidadesAleatorias() {
+        Random random = new Random();
+        for (Particula i : particulas) {
+            i.setVelocidadX(random.nextInt(RANGO_NUMEROS_ALEATORIOS));
+            i.setVelocidadY(random.nextInt(RANGO_NUMEROS_ALEATORIOS));
         }
     }
 
     public void funcionDeAdaptacion() {
         int posicionX, posicionY;
-        for(Particula p: particulas){
+        for (Particula p : particulas) {
             posicionX = p.getX();
             posicionY = p.getY();
             p.setValorMinimoFuncionObjetivo(funcionObjetivo(posicionX, posicionY));
         }
     }
-    
-    private Particula getParticulaMejorPosicion(){
+
+    private Particula getParticulaMejorPosicion() {
         Particula particula = particulas.get(0);
         int valorParticula;
         int valorMinimo = particula.getValorMinimoFuncionObjetivo();
-        for(Particula i : particulas){
+        for (Particula i : particulas) {
             valorParticula = i.getValorMinimoFuncionObjetivo();
-            if(valorParticula < valorMinimo){
+            if (valorParticula < valorMinimo) {
                 valorMinimo = valorParticula;
                 particula = i;
             }
         }
         return particula;
     }
-    
-    public void actualizarMejoresPosiciones(){
+
+    public void actualizarMejoresPosiciones() {
         int valorMinimoActual;
-        
-        for(Particula i : particulas){
+
+        for (Particula i : particulas) {
             valorMinimoActual = i.getValorMinimoFuncionObjetivo();
-            if(funcionObjetivo(i.getX(),i.getY()) < valorMinimoActual){
+            if (funcionObjetivo(i.getX(), i.getY()) < valorMinimoActual) {
                 i.setMejorPosicionX(i.getX());
                 i.setMejorPosicionY(i.getY());
             }
         }
     }
-    
-    public void actualizarPosicionesParticulas(){
+
+    public void actualizarPosicionesParticulas() {
         int nuevaPosicionX, nuevaPosicionY;
-        for(Particula i : particulas){
+        for (Particula i : particulas) {
             nuevaPosicionX = i.getX() + i.getVelocidadX();
             nuevaPosicionY = i.getY() + i.getVelocidadY();
             i.setX(nuevaPosicionX);
             i.setY(nuevaPosicionY);
         }
     }
-    
-    public void actualizarParticulaLider(){
+
+    public void actualizarParticulaLider() {
         int resultadoParticulaLider = funcionObjetivo(lider.getX(), lider.getY());
         int resultado;
-        for(Particula p : particulas){
+        for (Particula p : particulas) {
             resultado = funcionObjetivo(p.getX(), p.getY());
-            if(resultado < resultadoParticulaLider){
+            if (resultado < resultadoParticulaLider) {
                 lider = p;
             }
         }
     }
-    
-    public void actualizarVelocidadesParticulas(){
+
+    public void actualizarVelocidadesParticulas() {
         Random random = new Random();
-        for(Particula p : particulas){
-            p.setVelocidadX((int) ((velocidadInercial*p.getVelocidadX())+(aceleracionMinima*random.nextFloat())*
-                    (p.getMejorPosicionX()-p.getX()) + ((aceleracionMaxima*random.nextFloat())*
-                            (lider.getX()-p.getX()))));
-            p.setVelocidadY((int) ((velocidadInercial*p.getVelocidadY())+(aceleracionMinima*random.nextFloat())*
-                    (p.getMejorPosicionY()-p.getY()) + ((aceleracionMaxima*random.nextFloat())*
-                            (lider.getY()-p.getY()))));
-            System.out.println("Velocidad X: "+p.getVelocidadX());
-            System.out.println("Velocidad Y: "+p.getVelocidadY());
+        for (Particula p : particulas) {
+            p.setVelocidadX((int) ((velocidadInercial * p.getVelocidadX()) + (aceleracionMinima * random.nextFloat())
+                    * (p.getMejorPosicionX() - p.getX()) + ((aceleracionMaxima * random.nextFloat())
+                    * (lider.getX() - p.getX()))));
+            p.setVelocidadY((int) ((velocidadInercial * p.getVelocidadY()) + (aceleracionMinima * random.nextFloat())
+                    * (p.getMejorPosicionY() - p.getY()) + ((aceleracionMaxima * random.nextFloat())
+                    * (lider.getY() - p.getY()))));
+            System.out.println("Velocidad X: " + p.getVelocidadX());
+            System.out.println("Velocidad Y: " + p.getVelocidadY());
         }
     }
-    
-    public void calcularInerciaYVelocidades(int iteracionActual, int cantidadIteraciones){
-		velocidadInercial = ((INERCIA_MINIMA-INERCIA_MAXIMA)/(cantidadIteraciones-1)) * (iteracionActual-1)+INERCIA_MAXIMA;
-		aceleracionMinima = ((ACELERACION_MINIMA-ACELERACION_MAXIMA)/cantidadIteraciones)*iteracionActual+ACELERACION_MAXIMA;
-		aceleracionMaxima = ((ACELERACION_MAXIMA-ACELERACION_MINIMA)/cantidadIteraciones)*iteracionActual+ACELERACION_MINIMA;
-                System.out.println("C1: "+aceleracionMinima);
-                System.out.println("C2: "+aceleracionMaxima);
-                System.out.println("Velocidad Inercial: "+velocidadInercial);
-	}
+
+    public void calcularInerciaYVelocidades(int iteracionActual, int cantidadIteraciones) {
+        velocidadInercial = ((INERCIA_MINIMA - INERCIA_MAXIMA) / (cantidadIteraciones - 1)) * (iteracionActual - 1) + INERCIA_MAXIMA;
+        aceleracionMinima = ((ACELERACION_MINIMA - ACELERACION_MAXIMA) / cantidadIteraciones) * iteracionActual + ACELERACION_MAXIMA;
+        aceleracionMaxima = ((ACELERACION_MAXIMA - ACELERACION_MINIMA) / cantidadIteraciones) * iteracionActual + ACELERACION_MINIMA;
+        System.out.println("C1: " + aceleracionMinima);
+        System.out.println("C2: " + aceleracionMaxima);
+        System.out.println("Velocidad Inercial: " + velocidadInercial);
+    }
 
     public ArrayList<Particula> getEnjambre() {
         return particulas;
